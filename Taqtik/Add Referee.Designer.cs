d2fd0d7c8b6beb6b7ce1a1f7f29067c6a5c583d4
Omar Essam <Omar.Shady05@eng-st.cu.edu.eg>
2025-12-17 00:16:54 +0200

    yarab n5ls

---
+++ Taqtik/Add Referee.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Taqtik
+{
+    partial class Add_Referee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_Referee));
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_add = new System.Windows.Forms.Button();
+            this.textBox_country = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_age = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(963, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(306, 55);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Add Referee";
+            // 
+            // button_add
+            // 
+            this.button_add.ForeColor = System.Drawing.Color.Black;
+            this.button_add.Location = new System.Drawing.Point(1020, 561);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(163, 66);
+            this.button_add.TabIndex = 14;
+            this.button_add.Text = "Add";
+            this.button_add.UseVisualStyleBackColor = true;
+            // 
+            // textBox_country
+            // 
+            this.textBox_country.Location = new System.Drawing.Point(1033, 427);
+            this.textBox_country.Name = "textBox_country";
+            this.textBox_country.Size = new System.Drawing.Size(236, 48);
+            this.textBox_country.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(863, 430);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 40);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Country:";
+            // 
+            // textBox_age
+            // 
+            this.textBox_age.Location = new System.Drawing.Point(1033, 351);
+            this.textBox_age.Name = "textBox_age";
+            this.textBox_age.Size = new System.Drawing.Size(236, 48);
+            this.textBox_age.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(895, 354);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 40);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Age:";
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(1033, 270);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(236, 48);
+            this.textBox_name.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(895, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 40);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Name:";
+            // 
+            // Add_Referee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 40F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1478, 844);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.textBox_country);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_age);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Add_Referee";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add_Referee";
+            this.Load += new System.EventHandler(this.Add_Referee_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.TextBox textBox_country;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_age;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Taqtik/AddCompetitionInstance.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Taqtik
+{
+    partial class AddCompetitionInstance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddCompetitionInstance));
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox_Season = new System.Windows.Forms.ComboBox();
+            this.label_1 = new System.Windows.Forms.Label();
+            this.comboBox_competition = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_noOfTeams = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_relegation = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_promotion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(794, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(598, 55);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Add Competition Instance";
+            // 
+            // comboBox_Season
+            // 
+            this.comboBox_Season.FormattingEnabled = true;
+            this.comboBox_Season.Location = new System.Drawing.Point(1022, 202);
+            this.comboBox_Season.Name = "comboBox_Season";
+            this.comboBox_Season.Size = new System.Drawing.Size(285, 48);
+            this.comboBox_Season.TabIndex = 75;
+            // 
+            // label_1
+            // 
+            this.label_1.AutoSize = true;
+            this.label_1.BackColor = System.Drawing.Color.Transparent;
+            this.label_1.Location = new System.Drawing.Point(844, 205);
+            this.label_1.Name = "label_1";
+            this.label_1.Size = new System.Drawing.Size(149, 40);
+            this.label_1.TabIndex = 74;
+            this.label_1.Text = "Season:";
+            // 
+            // comboBox_competition
+            // 
+            this.comboBox_competition.FormattingEnabled = true;
+            this.comboBox_competition.Location = new System.Drawing.Point(1022, 289);
+            this.comboBox_competition.Name = "comboBox_competition";
+            this.comboBox_competition.Size = new System.Drawing.Size(285, 48);
+            this.comboBox_competition.TabIndex = 77;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(797, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 40);
+            this.label1.TabIndex = 76;
+            this.label1.Text = "Competition:";
+            // 
+            // textBox_noOfTeams
+            // 
+            this.textBox_noOfTeams.Location = new System.Drawing.Point(1022, 527);
+            this.textBox_noOfTeams.Name = "textBox_noOfTeams";
+            this.textBox_noOfTeams.Size = new System.Drawing.Size(236, 48);
+            this.textBox_noOfTeams.TabIndex = 83;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(762, 530);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 40);
+            this.label3.TabIndex = 82;
+            this.label3.Text = "No. of Teams:";
+            // 
+            // textBox_relegation
+            // 
+            this.textBox_relegation.Location = new System.Drawing.Point(1022, 451);
+            this.textBox_relegation.Name = "textBox_relegation";
+            this.textBox_relegation.Size = new System.Drawing.Size(236, 48);
+            this.textBox_relegation.TabIndex = 81;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(794, 454);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 40);
+            this.label2.TabIndex = 80;
+            this.label2.Text = "Relegation:";
+            // 
+            // textBox_promotion
+            // 
+            this.textBox_promotion.Location = new System.Drawing.Point(1022, 370);
+            this.textBox_promotion.Name = "textBox_promotion";
+            this.textBox_promotion.Size = new System.Drawing.Size(236, 48);
+            this.textBox_promotion.TabIndex = 79;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(802, 373);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 40);
+            this.label5.TabIndex = 78;
+            this.label5.Text = "Promotion:";
+            // 
+            // button_add
+            // 
+            this.button_add.ForeColor = System.Drawing.Color.Black;
+            this.button_add.Location = new System.Drawing.Point(970, 627);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(163, 66);
+            this.button_add.TabIndex = 84;
+            this.button_add.Text = "Add";
+            this.button_add.UseVisualStyleBackColor = true;
+            // 
+            // AddCompetitionInstance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 40F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1478, 894);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.textBox_noOfTeams);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_relegation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_promotion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox_competition);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_Season);
+            this.Controls.Add(this.label_1);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "AddCompetitionInstance";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddCompetitionInstance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox_Season;
+        private System.Windows.Forms.Label label_1;
+        private System.Windows.Forms.ComboBox comboBox_competition;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_noOfTeams;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_relegation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_promotion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_add;
+    }
+}
